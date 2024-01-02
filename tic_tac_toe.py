47ccd70909c32c2a1f8a0b4554cf6aeeb4c27f6c
Dan <127423122+Coding-Wright@users.noreply.github.com>
2024-01-02 16:33:22 +0000

    FIRST_STEP
    First step followed, seems to be only display related

---
+++ tic_tac_toe.py
@@ -0,0 +1,19 @@
+# https://realpython.com/tic-tac-toe-python/#step-1-set-up-the-tic-tac-toe-game-board-with-tkinter
+
+import tkinter as tk
+from tkinter import font
+
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tic-Tac-Toe Game")
+        self._cells = {}
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready?",
+            font=font.Font(size=28, weight="bold"),
+        )
+        self.display.pack
